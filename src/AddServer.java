abb836f6a01310b838deab43b1e9ca2e04f00d4b
m4n1c22 <sreeram.sadasivam@gmail.com>
2014-11-13 01:50:14 +0100

    finally working code...
    Changed the rebind name in addserver file. Call the client controller
    class file with parameter localhost

--- src/AddServer.java
+++ src/AddServer.java
@@ -4,7 +4,7 @@ public class AddServer {
 public static void main(String args[]) {
 try {
 AddServerImpl addServerImpl = new AddServerImpl();
-Naming.rebind("AddServer", addServerImpl);
+Naming.rebind("//localhost/AddServer", addServerImpl);
 }
 catch(Exception e) {
 System.out.println("Exception:  " + e);
